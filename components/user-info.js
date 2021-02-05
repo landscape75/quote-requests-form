8dabfa0244f688c61f8cafc9f1b13c0d5159645d
Dermot Fitzpatrick <39203833+tomredf@users.noreply.github.com>
2021-02-05 15:57:39 -0800

    ...

--- components/user-info.js
+++ components/user-info.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import userbase from 'userbase-js'
+import Link from 'next/link'
 
 function UserInfo({ user }) {
   const [username, setUsername] = useState('')
@@ -66,10 +67,14 @@ function UserInfo({ user }) {
     }
   }
 
+  function goBack() {
+    window.history.back();
+  }
+
   return (
 
-    <div className="flex items-center rounded-lg max-w-md  mx-auto shadow-lg justify-center bg-white dark:bg-mag-grey-600 pt-3 pb-4 px-4 mb-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-6">
+    <div className="flex items-center rounded-lg max-w-lg  mx-auto shadow-lg justify-center bg-white dark:bg-mag-grey-600 pt-3 pb-4 px-4 mb-4 sm:px-6 lg:px-8">
+      <div className="max-w-lg w-full space-y-6">
         <div>
           {/* <img className="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt=""/> */}
           <svg className="mx-auto h-12 w-12 text-mag-blue" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -83,58 +88,11 @@ function UserInfo({ user }) {
         </div>
         <form className="mt-8 space-y-6">
           <div className="rounded-md shadow-sm -space-y-px">
-            <div>
-              <label htmlFor="username" className="sr-only">Username</label>
-              <input id="username" 
-                name="username" 
-                type="text" 
-                required 
-                className="appearance-none rounded-none relative block w-full bg-white dark:bg-gray-100 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-mag-blue focus:border-mag-blue focus:z-10 sm:text-sm" 
-                placeholder="Username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </div>
+
 
               <>
-                <div>
-                  <label htmlFor="email-address" className="sr-only">Email Address</label>
-                  <input id="email-address" 
-                    name="email" 
-                    type="email" 
-                    autoComplete="email" 
-                    required 
-                    className="appearance-none rounded-none relative block w-full bg-white dark:bg-gray-100 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-mag-blue focus:border-mag-blue focus:z-10 sm:text-sm" 
-                    placeholder="Email Address" 
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </div>
-                <div>
-                  <label htmlFor="name" className="sr-only">Name</label>
-                  <input id="name" 
-                    name="name" 
-                    type="text" 
-                    required 
-                    className="appearance-none rounded-none relative block w-full bg-white dark:bg-gray-100 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-mag-blue focus:border-mag-blue focus:z-10 sm:text-sm" 
-                    placeholder="Full Name" 
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                  />
-                </div>
-                <div>
-                  <label htmlFor="city" className="sr-only">City</label>
-                  <input id="city" 
-                    name="city" 
-                    type="text" 
-                    required 
-                    className="appearance-none rounded-none relative block w-full bg-white dark:bg-gray-100 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-mag-blue focus:border-mag-blue focus:z-10 sm:text-sm" 
-                    placeholder="City" 
-                    value={city}
-                    onChange={(e) => setCity(e.target.value)}
-                  />
-                </div>
-                <div>
+
+{/*                 <div>
                   <label htmlFor="dbname" className="sr-only">Database Name</label>
                   <input id="dbname" 
                     name="dbname" 
@@ -145,63 +103,117 @@ function UserInfo({ user }) {
                     value={dbName}
                     onChange={(e) => setDBname(e.target.value)}
                   />
-                </div>
+                </div> */}
               </>
 
-            <div>
-              <label htmlFor="password" className="sr-only">Password</label>
-              <input id="password" 
-                name="password" 
-                type="password" 
-                autoComplete="current-password" 
-                required 
-                className="appearance-none rounded-none relative block w-full bg-white dark:bg-gray-100 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-mag-blue focus:border-mag-blue focus:z-10 sm:text-sm" 
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
+            <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 sm:gap-4 sm:items-start sm:pt-5">
+              <label htmlFor="username" className="block text-md font-medium text-gray-700 dark:text-gray-100 sm:mt-px sm:pt-2">
+                Username
+              </label>
+              <div className="mt-1 sm:mt-0 md:col-span-1 sm:col-span-3 lg:col-span-2">
+                <input 
+                  id="username" 
+                  name="username" 
+                  type="text"
+                  value={username}
+                  placeholder="Username"
+                  className="max-w-lg block bg-white focus:ring-mag-blue focus:border-mag-blue w-full shadow-md sm:max-w-xs sm:text-sm border-gray-300 dark:border-gray-500 rounded-md"
+                  onChange={(e) => setUsername(e.target.value)}
+                >
+                </input>
+              </div>
+            </div>
+
+            <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 sm:gap-4 sm:items-start sm:pt-5">
+              <label htmlFor="password" className="block text-md font-medium text-gray-700 dark:text-gray-100 sm:mt-px sm:pt-2">
+                Password
+              </label>
+              <div className="mt-1 sm:mt-0 md:col-span-1 sm:col-span-3 lg:col-span-2">
+                <input 
+                  id="password" 
+                  name="password" 
+                  type="password" 
+                  autoComplete="current-password" 
+                  value={password}
+                  placeholder="Username"
+                  className="max-w-lg block bg-white focus:ring-mag-blue focus:border-mag-blue w-full shadow-md sm:max-w-xs sm:text-sm border-gray-300 dark:border-gray-500 rounded-md"
+                  onChange={(e) => setPassword(e.target.value)}
+                >
+                </input>
+              </div>
+            </div>
+
+            <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 sm:gap-4 sm:items-start sm:pt-5">
+              <label htmlFor="email" className="block text-md font-medium text-gray-700 dark:text-gray-100 sm:mt-px sm:pt-2">
+                Email Address
+              </label>
+              <div className="mt-1 sm:mt-0 md:col-span-1 sm:col-span-3 lg:col-span-2">
+                <input 
+                  id="email" 
+                  name="email" 
+                  type="text"
+                  value={email}
+                  placeholder="Email Address"
+                  className="max-w-lg block bg-white focus:ring-mag-blue focus:border-mag-blue w-full shadow-md sm:max-w-xs sm:text-sm border-gray-300 dark:border-gray-500 rounded-md"
+                  onChange={(e) => setEmail(e.target.value)}
+                >
+                </input>
+              </div>
+            </div>
+
+            <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 sm:gap-4 sm:items-start sm:pt-5">
+              <label htmlFor="full-name" className="block text-md font-medium text-gray-700 dark:text-gray-100 sm:mt-px sm:pt-2">
+                Full Name
+              </label>
+              <div className="mt-1 sm:mt-0 md:col-span-1 sm:col-span-3 lg:col-span-2">
+                <input 
+                  id="full-name" 
+                  name="full-name" 
+                  type="text"
+                  value={name}
+                  placeholder="Full Name"
+                  className="max-w-lg block bg-white focus:ring-mag-blue focus:border-mag-blue w-full shadow-md sm:max-w-xs sm:text-sm border-gray-300 dark:border-gray-500 rounded-md"
+                  onChange={(e) => setName(e.target.value)}
+                >
+                </input>
+              </div>
+            </div>
+
+            <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 sm:gap-4 sm:items-start sm:pt-5">
+              <label htmlFor="city" className="block text-md font-medium text-gray-700 dark:text-gray-100 sm:mt-px sm:pt-2">
+                City
+              </label>
+              <div className="mt-1 sm:mt-0 md:col-span-1 sm:col-span-3 lg:col-span-2">
+                <input 
+                  id="city" 
+                  name="city" 
+                  type="text"
+                  value={city}
+                  placeholder="City"
+                  className="max-w-lg block bg-white focus:ring-mag-blue focus:border-mag-blue w-full shadow-md sm:max-w-xs sm:text-sm border-gray-300 dark:border-gray-500 rounded-md"
+                  onChange={(e) => setCity(e.target.value)}
+                >
+                </input>
+              </div>
             </div>
             
           </div>
 
-          <div className="flex items-center justify-between">
-            <div className="flex items-center">
-              <input id="remember_me" 
-                name="remember_me" 
-                type="checkbox" 
-                // value={rememberMe} 
-                //isChecked={rememberMe}
-                className="h-4 w-4 text-mag-blue focus:ring-mag-blue border-gray-300 rounded" 
-                onChange={(e) => handleCheck(e.target.checked)}
-              />
-              <label htmlFor="remember_me" className="ml-2 block text-sm text-gray-900 dark:text-white">
-                Remember me
-              </label>
-            </div>
-
-{/*             <div className="text-sm">
-              {modalType === 'logIn' &&
-                <a href="#" className="font-medium text-mag-blue hover:text-indigo-500">
-                  Forgot your password?
-                </a>
-              }
-            </div> */}
-          </div>
-
-          <div class="inline-flex w-full space-x-4 justify-center">
-
-              <button 
-                className="group relative w-1/3 flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-mag-blue hover:bg-mag-blue-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-mag-blue" 
-                disabled={loading} 
-                onClick={handleLogIn}
-              >
-                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                  <svg className="h-5 w-5 text-mag-blue group-hover:text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                    <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
-                  </svg>
-                </span>
-                Cancel
-              </button>
+          <div class="inline-flex w-full space-x-4 justify-right">
+              <Link href="/">
+                <button 
+                  className="group relative w-1/3 flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-mag-blue hover:bg-mag-blue-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-mag-blue" 
+                  disabled={loading} 
+                  //onClick={goBack}
+                >
+                  <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                    <svg className="h-5 w-5 text-mag-blue group-hover:text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
+                    </svg>
+                  </span>
+                     Cancel
+                </button>
+              </Link>
  
               <button 
                 className="group relative w-1/3 flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-mag-blue hover:bg-mag-blue-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-mag-blue" 
