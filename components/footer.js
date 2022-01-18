36810adaceebc2dcc1166484cbab2a364bb5425e
“landscape75” <it@landscapecentre.com>
2022-01-18 09:44:06 -0800

    updated sendgrid apikeys

--- components/footer.js
+++ components/footer.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 
 export default function Footer() {
   const [curYear, setYear] = useState("");
-  const [version] = useState("2022.01.14.A");
+  const [version] = useState("2022.01.18.A");
 
   useEffect(() => {
     setYear(new Date().getFullYear().toString());
